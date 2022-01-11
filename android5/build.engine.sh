4fb90769654d013058203a1ee77dd3022e782aea
Maks <maksim.kolesnik@fulldive.com>
2022-01-11 19:16:29 +0300

    Bumped up version 1.0.8. Added copyrights

--- android5/build.engine.sh
+++ android5/build.engine.sh
@@ -1,4 +1,16 @@
 #!/bin/shexport ANDROID_NDK_HOME=$ANDROID_HOME/ndk-bundle/
+#
+# This file is part of Blokada.
+#
+# This Source Code Form is subject to the terms of the Mozilla Public
+# License, v. 2.0. If a copy of the MPL was not distributed with this
+# file, You can obtain one at https://mozilla.org/MPL/2.0/.
+#
+# Copyright © 2022 Blocka AB. All rights reserved.
+#
+# @author Karol Gusak (karol@blocka.net)
+#
+
 cd ../blocka_engine
 
 echo "Building for arm"
